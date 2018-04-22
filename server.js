1624d799e8f59073ced426fa88489c358479b81f
Walter Wan <WalterHWan@gmail.com>
2018-04-21 21:36:19 -0700

    Adjust eslint setting

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ var bodyParser = require('body-parser');
 var app = express();
 var router = express.Router();
 
-var bodyParser = require('body-parser');
-
-
 //set our port to either a predetermined port number if you have set it up, or 3001
 var port = process.env.API_PORT || 8080;
 
