ee18d6f7a63442030110c2d2b181aba2edbe3666
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-26 10:39:00 -0700

    Update home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -36,10 +36,12 @@ class Home extends React.Component {
 
   render() {
     return (
-
-        <div className="home-area">
-          <button className="create-button">Create Your Team</button>
-          <button className="join-button">Join A Team</button>
+        <div className="home-page">
+          <p className="tag-location">TAG HERE</p>
+          <div className="home-area">
+            <button className="create-button">Create Your Team</button>
+            <button className="join-button">Join A Team</button>
+          </div>
         </div>
 
     )
