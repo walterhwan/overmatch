05334dbab40a4a90182207d74281e55bce9083eb
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-27 12:31:57 -0700

    Able to pass in user data to team page

--- src/player_info.jsx
+++ src/player_info.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Cookies from 'universal-cookie';
 const cookies = new Cookies();
 
@@ -6,17 +7,39 @@ class PlayerInfo extends React.Component {
   constructor(props) {
     super(props);
 
-    console.log(cookies.get('battleTag'));
+    this.state = {
+      userInfo: {}
+    }
+
+    this.getUserInfoByBattleTag = this.getUserInfoByBattleTag.bind(this);
+    this.getUserInfoByBattleTag(cookies.get('battleTag'));
   }
+
+  getUserInfoByBattleTag(battleTag) {
+    axios.defaults.port = 8080;
+    axios.post('http://localhost:8080/api/testing/', {
+      battleTag: battleTag
+    })
+      .then(res => {
+        this.setState({
+          userInfo: res.data
+        })
+      })
+  }
+
   render() {
+    let { userInfo } = this.state;
+    let { rank_img, rank } = userInfo.competitive || {};
+    console.log(userInfo);
+    // let username = userInfo.username;
     return (
       <div className='player-info'>
         <div className='player-info-slot smooth-border'>
-          <img className='portrait smooth-border' alt='portrait' src="https://d1u1mce87gyfbn.cloudfront.net/game/unlocks/0x0250000000001401.png"></img>
-          <p className='name' scrolling="no">{"A_freakin_lo".toUpperCase()}</p>
+          <img className='portrait smooth-border' alt='portrait' src={userInfo.portrait}></img>
+          <p className='name' scrolling="no">{userInfo.username}</p>
           <div className='rank_div'>
-            <img  className='rank_img'  alt='rank_img' src="https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/season-2/rank-5.png"></img>
-            <p className='rank'>2856</p>
+            <img  className='rank_img'  alt='rank_img' src={rank_img}></img>
+            <p className='rank'>{rank}</p>
           </div>
         </div>
         <ul className='favorite-heros'>
