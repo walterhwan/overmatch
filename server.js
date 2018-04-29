95567ea777c528b225d48f33a64d341902111c35
Walter Wan <WalterHWan@gmail.com>
2018-04-29 13:31:59 -0700

    Add everything back

--- server.js
+++ server.js
@@ -49,50 +49,50 @@ router.get('/', function(req, res) {
 app.use('/api', router);  // /api
 app.use('/api', userRouter);  // /api/users
 app.use('/api', teamRouter);  // /api/teams
-//
-// // The following is for oauth, backend route /api/test for now
-// app.post('/api/test', function(req, res) { // api/test
-//   // console.log(req.body);
-//   let authCode = req.body.authCode;
-//   var request = require("request");
-//   var options = { method: 'POST',
-//     url: 'https://us.battle.net/oauth/token',
-//     headers:
-//      { 'postman-token': '319cd41d-3df2-922d-f6d7-d5b22a9ceccf',
-//        'cache-control': 'no-cache',
-//        authorization: 'Basic cXc3cmp6cHVzdWpmbnJmcjYyODJ6NXZqOHp4dHFncGI6Z1J4aGNGUVRITlpUODVLU2JSc3hWQTRnMktuandRVVc=',
-//        'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
-//     formData:
-//      { grant_type: 'authorization_code',
-//        code: authCode,
-//        redirect_uri: 'https://1a6dad92.ngrok.io/home' } };
-//
-//   let accessToken;
-//   let battleTag;
-//   request(options, function (error, response, body) {
-//     if (error) throw new Error(error);
-//     accessToken = JSON.parse(body).access_token;
-//     // console.log("accessToken: " + accessToken);
-//
-//     var request2 = require("request");
-//     var options = { method: 'GET',
-//       url: 'https://us.api.battle.net/account/user',
-//       qs: { access_token: accessToken },
-//       headers:
-//        { 'Postman-Token': 'f3ed47ac-29c5-466e-9830-903e32f95894',
-//          'Cache-Control': 'no-cache' } };
-//
-//     request2(options, function (error, response, body) {
-//       if (error) throw new Error(error);
-//
-//       battleTag = JSON.parse(body).battletag
-//       res.json({battleTag: battleTag})
-//       // console.log("BACKEND:");
-//       console.log(body);
-//     });
-//   });
-// });
-//
+
+// The following is for oauth, backend route /api/test for now
+app.post('/api/test', function(req, res) { // api/test
+  // console.log(req.body);
+  let authCode = req.body.authCode;
+  var request = require("request");
+  var options = { method: 'POST',
+    url: 'https://us.battle.net/oauth/token',
+    headers:
+     { 'postman-token': '319cd41d-3df2-922d-f6d7-d5b22a9ceccf',
+       'cache-control': 'no-cache',
+       authorization: 'Basic cXc3cmp6cHVzdWpmbnJmcjYyODJ6NXZqOHp4dHFncGI6Z1J4aGNGUVRITlpUODVLU2JSc3hWQTRnMktuandRVVc=',
+       'content-type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' },
+    formData:
+     { grant_type: 'authorization_code',
+       code: authCode,
+       redirect_uri: 'https://1a6dad92.ngrok.io/home' } };
+
+  let accessToken;
+  let battleTag;
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    accessToken = JSON.parse(body).access_token;
+    // console.log("accessToken: " + accessToken);
+
+    var request2 = require("request");
+    var options = { method: 'GET',
+      url: 'https://us.api.battle.net/account/user',
+      qs: { access_token: accessToken },
+      headers:
+       { 'Postman-Token': 'f3ed47ac-29c5-466e-9830-903e32f95894',
+         'Cache-Control': 'no-cache' } };
+
+    request2(options, function (error, response, body) {
+      if (error) throw new Error(error);
+
+      battleTag = JSON.parse(body).battletag
+      res.json({battleTag: battleTag})
+      // console.log("BACKEND:");
+      console.log(body);
+    });
+  });
+});
+
 app.post('/api/testing/', function(req, res) {
   var request = require("request");
   let battleTag = req.body.battleTag.replace("#", "-");
