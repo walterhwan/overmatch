79daf130f3a0f8170e38f819852b4bc0895bb009
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-29 15:08:00 -0700

    Modify login button API_URL

--- src/splash.jsx
+++ src/splash.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const API_URL = "https://overmatch-api.herokuapp.com";
+
 class SplashPage extends React.Component {
   render() {
     return (
@@ -7,7 +9,7 @@ class SplashPage extends React.Component {
         <div className="center-content">
           <p className="welcome-msg">Create or Join Your Winning Team before You Sign in to the Watch!</p>
           <button className = "bnet-button">
-            <a className="bnet-link" href="http://localhost:8080/auth/bnet">
+            <a className="bnet-link" href={`${API_URL}/auth/bnet`}>
               Log in with Battle.net
             </a>
           </button>
