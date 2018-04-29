eac3153fa55605f049b95c560056b9f9a465bde6
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-29 16:31:11 -0700

    Update users controller to start with empty array

--- controller/users.js
+++ controller/users.js
@@ -14,7 +14,11 @@ exports.apiBattleTagPUT = function(req, res) {
     user.authCode = req.body.authCode || user.authCode;
     user.battleTag = user.battleTag;
     user.team_id = req.body.team_id;
-    // user.heros = user.heros.concat(req.body.heros);
+    if(req.body.heros) {
+      // clear the old heros history
+      user.heros = [];
+      user.heros = user.heros.concat(req.body.heros);
+    }
     // console.log(req.body.team_id);
     // console.log(user);
     //save user
