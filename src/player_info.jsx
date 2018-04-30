266f4e2c754ef58fa3f188e74520d63ada111dca
Yu-Jen Chang <juzen2003@gmail.com>
2018-04-29 17:44:02 -0700

    Remove unwanted comment

--- src/player_info.jsx
+++ src/player_info.jsx
@@ -84,42 +84,4 @@ class PlayerInfo extends React.Component {
   }
 }
 
-// switchRoleLogo(select) {
-//     const image = document.getElementsByName("logo-swap")[0];
-//     image.src = select.options[select.selectedIndex].value;
-//   }
-//
-//   render() {
-//     return (
-//       <li className='player smooth-border' id='player-1'>
-//         <div className='hero-div'>
-//           <h1>Hero</h1>
-//           <div className='hero-select'>
-//             <select className="hero-dropdown">
-//               <option value='Doomfist'>Doomfist</option>
-//               <option value='Genji'>Genji</option>
-//               <option value='McCree'>McCree</option>
-//               <option value='Pharah'>Pharah</option>
-//               <option value='Reaper'>Reaper</option>
-//             </select>
-//           </div>
-//         </div>
-//         <div className='role-div'>
-//           <h1>Role</h1>
-//           <div className='role-select'>
-//             <img className="role-icon" name="logo-swap" alt='icon' src=""></img>
-//             <select className="role-dropdown" onChange={this.switchRoleLogo}>
-//               <option value='../images/tankIcon.png'>Tank</option>
-//               <option value='../images/supportIcon.png'>Support</option>
-//               <option value='../images/offenseIcon.png'>Offense</option>
-//               <option value='../images/defenseIcon.png'>Defense</option>
-//             </select>
-//           </div>
-//         </div>
-//         <PlayerStats battleTag={this.state.battleTag || ""}/>
-//       </li>
-//     );
-//   }
-// }
-
 export default withRouter(PlayerInfo);
